9dfadca2a04e69734f3d5bdb203905dc5bea82f8
jones-sam <samjones6058@gmail.com>
2020-06-12 17:07:35 -0300

    algolia search setup

--- src/components/common/ItemCard.jsx
+++ src/components/common/ItemCard.jsx
@@ -3,22 +3,23 @@ import Card from "react-bootstrap/Card"
 import ListGroup from "react-bootstrap/ListGroup"
 import ListGroupItem from "react-bootstrap/ListGroupItem"
 import { Link } from "react-router-dom"
+import Col from "react-bootstrap/Col"
 
 export class ItemCard extends Component {
   render() {
-    const { item } = this.props
+    const { hit } = this.props
     return (
       <Card className="align-self-center">
-        <Link to={`/items/${item.itemId}`}>
+        <Link to={`/items/${hit.objectID}`}>
           <Card.Img
             variant="top"
-            src={item.itemImage}
+            src={hit.itemImage}
             height="200"
             // width="250"
           />
           <Card.Body>
-            <Card.Title>{item.name}</Card.Title>
-            <Card.Text>${item.price}</Card.Text>
+            <Card.Title>{hit.name}</Card.Title>
+            <Card.Text>${hit.price}</Card.Text>
           </Card.Body>
         </Link>
       </Card>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,31 +5,88 @@ import ItemCard from "../components/common/ItemCard"
 import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
+import Form from "react-bootstrap/Form"
 
+// redux
 import { connect } from "react-redux"
 import { getItems } from "../redux/actions/dataActions"
+
+// Algolia
+import algoliasearch from "algoliasearch"
+import {
+  InstantSearch,
+  SearchBox,
+  Hits,
+  RefinementList,
+} from "react-instantsearch-dom"
+
+const client = algoliasearch(
+  process.env.REACT_APP_ALGOLIA_APP_ID,
+  process.env.REACT_APP_ALGOLIA_SEARCH_KEY
+)
+const itemIndex = client.initIndex("items")
+let itemHits = []
 class home extends Component {
+  state = {
+    searchQuery: "",
+  }
+
   componentDidMount() {
     this.props.getItems()
+    itemHits = this.props.data.items
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    console.log(`Submitted Search "${this.state.searchQuery}"`)
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value }, () => {
+      console.log(`searchQuery: ${this.state.searchQuery}`)
+      itemIndex.search(this.state.searchQuery).then((res) => {
+        itemHits = res.hits
+        console.log(res.hits)
+      })
+    })
   }
 
   render() {
     const { items, loading } = this.props.data
 
-    const listOfItems = !loading
-      ? items.map((item, index) => (
-          <Col key={item.itemId} lg={4} className="mb-4">
-            <ItemCard item={item} />
-          </Col>
-        ))
-      : null
+    // const listOfItems = !loading
+    //   ? items.map((item, index) => (
+    //       <Col key={`item${index}`} lg={4} className="mb-4">
+    //         <ItemCard item={item} />
+    //       </Col>
+    //     ))
+    //   : null
 
     return (
       <Container>
-        <Row className="mb-5">
+        {/* <Row className="mb-2">
           <h1>Frank's Furniture - The Best in Town</h1>
-        </Row>
-        <Row>{listOfItems}</Row>
+        </Row> */}
+        <InstantSearch searchClient={client} indexName="items" refresh>
+          <Row>
+            <SearchBox className="w-100 mb-3" />
+          </Row>
+          <Hits hitComponent={ItemCard} />
+        </InstantSearch>
+        {/* <Form
+            className="w-100 mb-5 d-flex justify-content-center"
+            onSubmit={this.handleSubmit}
+          >
+            <Form.Control
+              onChange={this.handleChange}
+              name="searchQuery"
+              size="lg"
+              type="text"
+              placeholder="Search"
+              className="w-75"
+            />
+          </Form> */}
+        {/* <Row>{listOfItems}</Row> */}
       </Container>
     )
   }
